72bfa1da06922339650d88a74e437c94d7628bc3
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-03-27 12:20:38 -0700

    add /updateContent

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, redirect, url_for, request, abort, session
-from datetime import timedelta
+from flask import Flask, redirect, url_for, request, abort, session, jsonify
+from datetime import timedelta, datetime
 from enum import Enum
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = "appsecretkey"
 app.permanent_session_lifetime = timedelta(days=30)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///shows.sqlite3"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app-db.sqlite3"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 isAdmin = True
@@ -15,10 +15,11 @@ isAdmin = True
 
 # Data Models
 class Content(db.Model):
-    _id = db.Column("id", db.Integer, primary_key=True)
+    id = db.Column("id", db.Integer, primary_key=True)
     title = db.Column(db.String(200))
     description = db.Column(db.String(1000))
     genre = db.Column(db.String(100))
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __init__(self, title, description, genre):
         self.title = title
@@ -27,10 +28,11 @@ class Content(db.Model):
 
 
 class User(db.Model):
-    _id = db.Column("id", db.Integer, primary_key=True)
-    username = db.Column(db.String(100))
-    email = db.Column(db.String(100))
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
     watchHistory = db.Column(db.String(100))
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __init__(self, username, email, watchHistory):
         self.username = username
@@ -70,8 +72,19 @@ def browse():
     loginIfNoSession()
 
     sessionData = session["username"]
-    content = Content.query.all()
-    return f"The Browse Page for {sessionData} \n {content[0].title}"
+    contentData = Content.query.all()
+
+    content = []
+    for c in contentData:
+        content_data = {
+            "id": c.id,
+            "title": c.title,
+            "description": c.description,
+            "genre": c.genre,
+        }
+        content.append(content_data)
+
+    return f"The Browse Page for {sessionData} \n{content}"
 
 
 @app.route("/yourAccount", methods=["GET", "POST"])
@@ -85,14 +98,16 @@ def yourAccount():
 @app.route('/viewData/<data>')
 def viewData(data):
     return f"Your Data: {data}"
+    # redirect(url_for("param", param="Hello Admin"))
 
 
+# Admin Protected Endpoints
 @app.route("/admin")
 def admin():
     if not isAdmin:
         abort(404)
 
-    return redirect(url_for("param", param="Hello Admin"))
+    return f"Hello Admin"
 
 
 @app.route("/addContent", methods=["GET", "POST"])
@@ -100,19 +115,40 @@ def addContent():
     if not isAdmin:
         abort(404)
 
-    loginIfNoSession()
-
     if request.method == "POST":
         title = request.form["title"]
+        description = request.form["description"]
+        genre = request.form["genre"]
 
         foundContent = Content.query.filter_by(title=title).first()
         if foundContent:
             return f"Content with title {title} already exists"
 
-        content = Content(title)
+        content = Content(title, description, genre)
         db.session.add(content)
         db.session.commit()
-        return f"Content added: {title}"
+        return f"Content added: {content}"
+
+    return f"The Add Content Page"
+
+
+@app.route("/updateContent", methods=["GET", "POST"])
+def updateContent():
+    if not isAdmin:
+        abort(404)
+
+    if request.method == "POST":
+        id = request.form["id"]
+        foundContent = Content.query.filter_by(id=id).first()
+        if not foundContent:
+            return f"Content with id {id} not found"
+
+        foundContent.title = request.form["title"]
+        foundContent.description = request.form["description"]
+        foundContent.genre = request.form["genre"]
+
+        db.session.commit()
+        return f"Content updated: {foundContent}"
 
     return f"The Add Content Page"
 
