024f199822395d15f6d14031f6805205535fd1b8
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-03-28 14:57:53 -0700

    start login page

---
+++ client/src/page-views/Login.js
@@ -0,0 +1,12 @@
+function Login() {
+    return (
+        <div className="login">
+            <header className="login-header">
+                <a>Login Logo</a>
+            </header>
+
+        </div>
+    );
+}
+
+export default Login;
