9f51ac67c1f4b5cfa47800324c4715b734bd8f4f
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-03-30 16:42:05 -0700

    begin Carousel

--- client/src/page-views/Feed.js
+++ client/src/page-views/Feed.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import apiURL from "../api";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 function Feed(props) {
     const { session } = props
@@ -16,6 +18,7 @@ function Feed(props) {
                 },
                 body: JSON.stringify({ "token": session.token })
             });
+
             setFeed(await response.json())
 
         } catch (err) {
@@ -27,11 +30,42 @@ function Feed(props) {
         fetchFeed();
     }, [])
 
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
+    function sections(name) {
+        <Carousel responsive={responsive}>
+            <div>Item 1</div>
+            <div>Item 2</div>
+            <div>Item 3</div>
+            <div>Item 4</div>
+        </Carousel>;
+    }
+
     return (<div className="feed">
         <header className="feed-header">
             <Link to="/" className="logo">Movies</Link>
         </header>
-        <p>Your Feed</p>
+        <div className="feed-body">
+
+        </div>
     </div>
     )
 }
